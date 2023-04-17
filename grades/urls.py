3547faffa6ac2191c1512d3cff1b89535546eaeb
Thamires Schifini Froelich <thamiresf@anchorloans>
2023-04-16 21:13:29 -0300

    Change function names to snake case

--- grades/urls.py
+++ grades/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     # ex: /polls/
-    path("", views.studentsList, name="students list"),
+    path("", views.students_list, name="students list"),
     # ex: /polls/5/
-    path("<int:student_id>/student_subjects/", views.studentSubjects, name="subjects"),
-    path("<int:subject_id>/subject_assignments/", views.subjectAssignments, name="student assignment grades")
+    path("<int:student_id>/student_subjects/", views.student_subjects, name="subjects"),
+    path("<int:subject_id>/subject_assignments/", views.subject_assignments, name="student assignment grades")
 ]
 
--- grades/views.py
+++ grades/views.py
@@ -4,7 +4,7 @@ from django.template import loader
 
 from .models import Student
 
-def studentsList(request):
+def students_list(request):
     all_students_list = Student.objects.all()
     template = loader.get_template("grades/studentsList.html")
     context = {
@@ -13,10 +13,10 @@ def studentsList(request):
     return HttpResponse(template.render(context, request))
 
 
-def studentSubjects(request, student_id=None):
+def student_subjects(request, student_id=None):
     return HttpResponse(f"this are the student {student_id} subjects" )
 
-def subjectAssignments(request, student_id, subject_id, grades_id=None):
+def subject_assignments(request, student_id, subject_id, grades_id=None):
     return HttpResponse(f"this is {subject_id} assignments {grades_id} of {student_id} ")
 
 
